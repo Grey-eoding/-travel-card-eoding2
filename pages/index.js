93e2491161fdfdd4f0a92d5ef471c1411df80ae1
Grey-eoding <dahye@eoding.com>
2025-07-09 00:08:29 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.container}>
       <h1>✈️ 어딩 트래블 카드 테스트</h1>
       <p className={styles.intro}>
-        ✈️ 여행 준비의 시작,<br />
+        여행 준비의 시작,<br />
         💳 나에게 딱 맞는 트래블 카드부터 골라보세요.<br />
         어딩이 만든 1분 테스트로<br />
         혜택·스타일·사용법까지 내 여행 성향에 맞는 카드 찾기!
