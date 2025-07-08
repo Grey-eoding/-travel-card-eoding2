ebf5ee905b2dac7176dd0caa6f283751099fa23c
Grey-eoding <dahye@eoding.com>
2025-07-09 00:07:28 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   const router = useRouter();
   return (
     <div className={styles.container}>
-      <h1>✈️ 어딩의 트래블 카드 테스트</h1>
+      <h1>✈️ 어딩 트래블 카드 테스트</h1>
       <p className={styles.intro}>
         ✈️ 여행 준비의 시작,<br />
         💳 나에게 딱 맞는 트래블 카드부터 골라보세요.<br />
