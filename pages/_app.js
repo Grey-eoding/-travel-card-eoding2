d5150e0181e0349144bc95ddc7d722b220ad4bc4
Grey-eoding <dahye@eoding.com>
2025-07-09 10:54:03 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
-// pages/_app.js
-import '../styles/globals.css'
+import '@/styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>여행 카드 테스트</title>
+        {/* Pretendard 웹폰트 로드 (혹은 Noto Sans KR 등으로 교체 가능) */}
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
