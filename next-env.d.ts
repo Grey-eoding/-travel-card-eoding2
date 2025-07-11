5306850c86b6713c2206696ee20301b45206e38d
Grey-eoding <dahye@eoding.com>
2025-07-11 16:22:11 +0900

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: 이 파일은 자동으로 수정되거나 삭제되면 안 됩니다.
+// TypeScript 환경설정을 위해 Next.js에서 필요합니다.
