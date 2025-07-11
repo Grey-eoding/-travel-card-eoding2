e53ff08cd16cb3d1fba29bfb18eacc81199fb5f2
Grey-eoding <dahye@eoding.com>
2025-07-11 16:43:20 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/travel-card-eoding2',
-  assetPrefix: '/travel-card-eoding2/',
+  output: 'export', // 정적 HTML 생성
+  basePath: '/travel-card-eoding2', // GitHub Pages용 하위 경로
+  assetPrefix: '/travel-card-eoding2/', // 정적 자산 경로 설정
+  trailingSlash: true, // export 시 경로 오류 방지를 위해 슬래시 붙이기 (권장)
 };
 
 module.exports = nextConfig;
