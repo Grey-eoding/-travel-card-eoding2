d9c768817d9e98abe57a0fd1caca317a61de2526
Grey-eoding <dahye@eoding.com>
2025-07-11 16:01:10 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  trailingSlash: true, // optional: export 할 때 경로 오류 방지
-}
+  output: 'export',
+  basePath: '/travel-card-eoding2',
+  assetPrefix: '/travel-card-eoding2/',
+};
 
 module.exports = nextConfig;
