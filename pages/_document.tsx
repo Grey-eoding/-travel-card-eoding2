615f7befb2ba296ec173c6c9efb6dfff82b68bac
Grey-eoding <dahye@eoding.com>
2025-07-11 16:59:55 +0900

    Update and rename _document.js to _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+// pages/_document.tsx
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="ko">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
